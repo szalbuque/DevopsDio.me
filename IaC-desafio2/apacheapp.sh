422e3e596a38a392e8e53e54826c376b347842cb
Silvia Rocha <szalbuque@gmail.com>
2022-10-18 11:00:08 -0300

    desafio 2

--- IaC-desafio2/apacheapp.sh
+++ IaC-desafio2/apacheapp.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-echo "Atualizando o servidor..."
-sudo su
-apt update
-apt upgrade -y
+if [ "$(id -u)" != "0" ]; then echo echo "Voce deve executar este script com poderes de root! " else 
 
-echo "Instalando o Apache..."
+	echo "Atualizando o servidor..."
+	apt update
+	apt upgrade -y
 
-echo "Instalando o Unzip..."
+	echo "Instalando o Apache..."
+	apt install apache2 -y
 
-echo "Clonando a aplicação..."
+	echo "Instalando o Unzip..."
+	apt install unzip -y
 
-echo "Copiando arquivos para o Apache..."
+	echo "Clonando a aplicação..."
+	cd /tmp/
+	wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+	unzip main.zip
+
+	echo "Copiando arquivos para o Apache..."
+	cd linux-site-dio-main
+	cp * /var/www/html/
 
 
+fi
