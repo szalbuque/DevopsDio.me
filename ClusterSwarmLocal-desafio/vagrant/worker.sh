10d815fbc0188dea29185c7376d7a4cf0b43b93e
Silvia Rocha <szalbuque@gmail.com>
2022-11-03 14:13:16 -0300

    desafio docker swarm finalizado

--- ClusterSwarmLocal-desafio/vagrant/worker.sh
+++ ClusterSwarmLocal-desafio/vagrant/worker.sh
@@ -1,1 +1,1 @@
-    docker swarm join --token SWMTKN-1-3bt5z7x1uklv1v04bvrqkapa61ifgkskr7kr2t619wh1gmc8yw-8l5p0npiz08lo3x2tab3t31jl 10.10.10.100:2377
+    docker swarm join --token SWMTKN-1-50j5qcsww731lur69c318rmwrqkywg4bn2efa4ge2rt1ximok5-daylth1yee5ttzu7mk1g2pfz8 10.10.10.100:2377
