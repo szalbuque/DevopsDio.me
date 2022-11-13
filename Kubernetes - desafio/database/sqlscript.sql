07512c3e44086d02322c223a70bc9aee2333d1e2
Silvia Rocha <szalbuque@gmail.com>
2022-11-13 14:37:38 -0300

    preparando o desafio kubernetes usando NFS filestore do gcp

---
+++ Kubernetes - desafio/database/sqlscript.sql
@@ -0,0 +1,8 @@
+CREATE TABLE mensagens (
+id int,
+nome varchar(50),
+email varchar(50),
+comentario varchar(100)
+);
+
+INSERT INTO mensagens (id, nome, email, comentario) VALUES (1,"Teste de usuario","usuario@teste.com","primeiro teste");
--- Kubernetes - desafio/script.sh
+++ Kubernetes - desafio/script.sh
@@ -2,15 +2,24 @@
 echo “Criando as imagens...”
 
 docker build -t szalbuque/projetobackend:1.0 backend/.
-docker build -t szalbuque/projeto-database:1.0 database/.
+docker build -t szalbuque/dbase:1.0 database/.
 
 echo “Fazendo o upload das imagens para o Docker Hub”
 
 docker push szalbuque/projetobackend:1.0
-docker push szalbuque/projeto-database:1.0
+docker push szalbuque/dbase:1.0
 
 echo “Criando os serviços no cluster kubernetes...”
 kubectl apply -f ./services.yml
 
-echo “Fazendo os deployments...”
+echo "Implantando a especificação do Persistent Volume..."
+kubectl create -f ./persistentvolume.yml
+
+echo "Implantando a especificação do Persistent Volume Claim"
+kubectl create -f ./pvc.yml
+
+echo “Fazendo o deploy do banco de dados...”
+kubectl apply -f ./dbasedeploy.yml
+
+echo "Fazendo o deploy da aplicação backend..."
 kubectl apply -f ./deployment.yml
